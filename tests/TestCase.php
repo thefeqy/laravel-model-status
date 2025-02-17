7b54b908871914f882344988ba0a10728cae9f40
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-17 21:21:46 +0200

    test: Setup PHPUnit

---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Thefeqy\ModelStatus\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use Thefeqy\ModelStatus\LaravelModelStatusServiceProvider;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Load package service provider.
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelModelStatusServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Set up test database.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('database.default', 'sqlite');
+        config()->set('database.connections.sqlite', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        $this->artisan('migrate');
+    }
+}

67e7c47d137b70047c5373958e45819582a3ceae
Muhammed ElFeqy <thefeqy@gmail.com>
2025-02-17 18:23:43 +0200

    feat: Add isActive() and isInactive methods to the Status.php Enum

--- src/Enums/Status.php
+++ src/Enums/Status.php
@@ -21,4 +21,20 @@ enum Status: string
     {
         return Config::get('model-status.inactive_value', 'inactive');
     }
+
+    /**
+     * Check if this string value matches the active status.
+     */
+    public function isActive(): bool
+    {
+        return $this->value === self::active();
+    }
+
+    /**
+     * Check if this string value matches the inactive status.
+     */
+    public function isInactive(): bool
+    {
+        return $this->value === self::inactive();
+    }
 }
